1754bf4ce9951c8be480f22e08917ad667ae9c04
PetriFS <petriferreira@hotmail.com>
2025-10-25 01:36:44 -0300

    Update desafio.kt
    Primeiro projeto do curso Kothin da Dio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,106 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+/*Escola de idiomas YeasyTell
+ * 
+ * Comecei sem usar a classe estilo enum, pela dificuldade que estava tendo em inclementar o programa, 
+ * porém após introduzir a classe enum, consegui entender o seu uso na pratica, pois não entendia o conceito
+ * que a mesma tinha. Pude constatar a praticidade de usa-la*/
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+enum class NivelCurso {   //então mantive a ideia de inicio, colocando a classe no nível
+    BÁSICO,
+    INTERMEDIÁRIO,
+    AVANÇADO
 }
 
+/*A classe Curso poderia ter mais parametros, como o conteúdo, programação, grade de materias*/
+
+data class Curso(
+    val nivel: NivelCurso, 
+    val duracaoMeses: Int,
+    val modulos: List<Modulo> 
+    )
+/*Usei o comando List para modulo, porém acredito que poderia ter uma outra maneira caso o programa fosse 
+ * mais elaborado, facilitando a manipulação do objeto*/
+
+data class Modulo(val nome: String)
+
+/*A classe Usuario poderia ter muitos parametros acrescentados em caso de um programa usual, número de
+ * documento, matricula, idade, e demais dados de cadastro chamando
+ * uma função POO para validar dados, bem como usuários do próprio sistema*/
+
+data class Usuario(val nome: String, val cursosInscritos: List<Curso>, val moduloInscrito: List<Modulo>)
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val modulos = listOf(
+        Modulo("Módulo 1"),
+        Modulo("Módulo 2"),
+        Modulo("Módulo 3"),
+        Modulo("Módulo 4"),
+        Modulo("Módulo 5"),
+    )
+    
+    val cursoBasico = Curso(NivelCurso.BÁSICO, 3, modulos)
+    val cursoIntermediario = Curso(NivelCurso.INTERMEDIÁRIO, 3, modulos)
+    val cursoAvancado = Curso(NivelCurso.AVANÇADO, 3, modulos)
+    
+    //cadastrando os primeiros usuarios
+    //o cadastro poder
+  
+    val usuario1 = Usuario("João", listOf(cursoBasico), listOf(modulos[2]))
+    val usuario2 = Usuario("Maria", listOf(cursoIntermediario), listOf(modulos[0]))
+    
+    /*Aqui tive mais um aprendizado na pratica, pois fui atualizar a lista e aprendi a diferença
+     * entre mutableListOf e ListOf :)*/
+    val usuarios = mutableListOf(usuario1, usuario2)
+    
+    val instituicaoCursos = listOf(cursoBasico, cursoIntermediario, cursoAvancado)
+    
+    /*Listando os cursos de inglês, poderia acrescentar outros cursos, com número de modulos e duração
+     * diferente usando a base das classes*/
+    
+    println("Cursos de Inglês da Instituição YeasyTell:")
+    instituicaoCursos.forEach { 
+        curso ->println("${curso.nivel}(${curso.duracaoMeses} meses)")
+        curso.modulos.forEach { modulo ->println(" - ${modulo.nome}")}
+
+    }
+    
+    // Listando os usuários, nível do curso e modulo
+    
+    println("\nUsuários e seus cursos e módulos inscritos:\n")
+    usuarios.forEach { 
+        usuario ->print("${usuario.nome} está cursando o ")
+        usuario.cursosInscritos.forEach { 
+            curso ->print("nível ${curso.nivel}")
+        }
+    usuario.moduloInscrito.forEach { 
+        modulo ->println(" no ${modulo.nome}")
+    }
+    println("----------------------------------------------------------------------------------")
+}
+    
+    
+    //testando mais funções
+    println("\n Atualizações: \n")
+    
+    //Adicionando mais usuarios
+    val usuario3 = Usuario("Alexandre", listOf(cursoAvancado), listOf(modulos[4])) 
+    usuarios.add(usuario3)
+    val usuario4 = Usuario("Marcia", listOf(cursoIntermediario), listOf(modulos[1])) 
+    usuarios.add(usuario4)
+    
+    //Atualizando, agora Maria está no modulo 2
+    usuarios[1]= Usuario("Maria", listOf(cursoIntermediario), listOf(modulos[1]))
+    
+    //Mostrando as atualizações
+   println("\nUsuários e seus cursos e módulos inscritos (atualizado):\n")
+    usuarios.forEach { 
+        usuario ->print("${usuario.nome} está cursando o ")
+        usuario.cursosInscritos.forEach { 
+            curso ->print("nível ${curso.nivel}")
+        }
+    usuario.moduloInscrito.forEach { 
+        modulo ->println(" no ${modulo.nome}")
+    }
+    println("----------------------------------------------------------------------------------")
+    }
+    
 }
